498a66c317e722aeca6917261341b7bff474011a
sammy171207 <sameerrandive171207@gmail.com>
2024-06-23 12:16:51 +0530

    this addd

--- src/main/java/com/example/Agent/AgentApplication.java
+++ src/main/java/com/example/Agent/AgentApplication.java
@@ -8,7 +8,7 @@ public class AgentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgentApplication.class, args);
-		System.out.println("Working");
+		System.out.println("Working upgit ");
 	}
      
 }
